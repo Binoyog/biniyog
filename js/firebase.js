e200b29961d989fc9bfe9ae81d8e7715a6a9784d
Binoyog <sowrovahmed6117@gmail.com>
2025-12-29 18:38:25 +0600

    Create firebase.js
    Signed-off-by: Binoyog <sowrovahmed6117@gmail.com>

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "654321",
+  authDomain: "biniyog-95d00.firebaseapp.com",
+  projectId: "biniyog-95d00",
+  storageBucket: "biniyog-95d00.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
